5dcc7af9b25777d56d65adc73f3b0c98169cd405
Nirvana <37197172+altcnews@users.noreply.github.com>
2018-03-17 07:44:58 +0800

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
-set(CN_PROJECT_NAME "Nirvanawallet")
+set(CN_PROJECT_NAME "Nirvana")
 set(CN_CURRENCY_DISPLAY_NAME "Nirvana")
 set(CN_CURRENCY_TICKER "VVC")
-set(CN_VERSION 2.0.2)
+set(CN_VERSION 1.0.1)
